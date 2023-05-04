5b48bb00454ee2e1046a71ca3b3d7f0ea3530481
GalileoCap <galileocapp@gmail.com>
2023-05-04 15:59:29 -0300

    CLEAN: Reached old compiler

--- src/compiler/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-nasm -felf64 main.asm && ld main.o && ./a.out; echo $?
-rm -f main.o a.out
--- src/main.py
+++ src/main.py
@@ -4,21 +4,40 @@ from expr import Expr
 
 if __name__ == '__main__':
   main = Expr.construct(('file', 'main.u',
+    # int64 globA = 0
     ('def', 'globA', 'int64', ('int64', 0)),
+
+    # int64 globB = 1
     ('def', 'globB', 'int64', ('int64', 1)),
+
+    # fun getGlob() int64 {
     ('fun', 'getGlob', 'int64', (),
+      # return globA
       ('return', ('var', 'globA')),
-    ),
-    ('fun', 'setGlob', 'int64', (('param', 'newValue', 'int64', ('call', 'getGlob')),),
+    ), # }
+
+    # fun setGlob(int64 newValue = getGlob())) {
+      ('fun', 'setGlob', 'int64', (('param', 'newValue', 'int64', ('call', 'getGlob')),),
+      # globA = newValue
       ('set', ('var', 'globA'), ('var', 'newValue')),
-    ),
+    ), # }
+    # fun main() int64 {
     ('fun', 'main', 'int64', (),
-      ('def', 'ing', '', ('int64', 4)),
-      ('set', ('var', 'ing'), ('int32', 5)),
+      # var ing = 2
+      ('def', 'ing', '', ('int64', 2)),
+
+      # ing = 3 # TODO: int32
+      ('set', ('var', 'ing'), ('int32', 3)),
+
+      # setGlob()
       ('call', 'setGlob'),
+
+      # ing = getGlob()
       ('set', ('var', 'ing'), ('call', 'getGlob')),
+
+      # return ing
       ('return', ('var', 'ing')),
-    ),
+    ), # }
   ))
 
   Execute(main)
