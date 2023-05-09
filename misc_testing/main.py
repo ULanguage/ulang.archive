996d3adfe13e65818ee7bc5f49acd95672cb2cf8
GalileoCap <galileocapp@gmail.com>
2023-05-09 01:08:53 -0300

    OK: Testing basic function parsing

--- misc_testing/main.py
+++ misc_testing/main.py
@@ -8,20 +8,33 @@ class UParser(lrparsing.Grammar):
         eol = Choice('\n', ';')
 
     expr = Ref('expr')
-    atom = T.ident | T.integer | '(' + expr + ')' | '()'
-
+    atom = T.ident | T.integer | '(' + expr + ')'
     block = '{' + Repeat(T.eol) + Repeat(expr + Repeat(T.eol)) + '}'
-    file = Repeat(T.eol) + Repeat(block + Repeat(T.eol))
+
+    fun = Keyword('fun') + T.ident + '(' + List(T.ident, ',', min = 0) + ')' + block
+    file = Repeat(T.eol) + Repeat((block | fun) + Repeat(T.eol))
 
     expr = Prio(
       atom,
       block,
+      fun,
     )
     START = file
-    COMMENTS = Token(re='/[*](?:[^*]|[*][^/])*[*]/')
+    COMMENTS = (
+      Token(re='/[*](?:[^*]|[*][^/])*[*]/') |
+      Token(re='//[^\n]*') # TODO: ^T.eol
+    )
     # WHITESPACE = ' '
 
 # parse_tree = UParser.parse('/* This is an\n * Example\n */\nfun main() int32 {\n\ttest() // Call a function\n  return 0\n}')
 # parse_tree = UParser.parse('ia = test(a)\ning = test(a, b, c) /* Call a function */')
-parse_tree = UParser.parse('\n{\na\n\tb\nc}\n{\na\t\nb}')
+parse_tree = UParser.parse('''
+fun main (a, b) {
+a
+  b // This is comment*/
+c}
+{
+a
+b}
+''')
 print(UParser.repr_parse_tree(parse_tree))
